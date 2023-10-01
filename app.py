49e01a4621206942d0c2679b51f14e080ad175b2
Sami Sultan <samisultan22@hotmail.co.uk>
2023-10-01 18:53:33 +0100

    Addition of new HTML files
    Addiiton of-
    - modify_recipe.html
    - new_resipe.html

--- app.py
+++ app.py
@@ -12,5 +12,13 @@ def home():
 def stock():
     return render_template("stock.html")
 
+@app.route("/newrecipe/")
+def newrecipe():
+    return render_template("new_recipe.html")
+
+@app.route("/modifyrecipe/")
+def modifyrecipe():
+    return render_template("modify_recipe.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
